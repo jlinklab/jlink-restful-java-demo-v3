19bbada99fe33c43e6eba56123b3ac6bbbb44e0f
hejieming <hejieming@xiongmaitech.com>
2025-09-15 16:46:37 +0800

    Update DeviceToken Methods In V3

--- src/main/java/jlink/restful/java/sdk/module/devicetoken/DeviceTokenRequest.java
+++ src/main/java/jlink/restful/java/sdk/module/devicetoken/DeviceTokenRequest.java
@@ -7,59 +7,18 @@ import jlink.restful.java.sdk.competent.JLinkDomain;
 import jlink.restful.java.sdk.competent.JLinkMethodType;
 import jlink.restful.java.sdk.competent.JLinkResponseCode;
 import jlink.restful.java.sdk.exception.JLinkDeviceInfoException;
-import jlink.restful.java.sdk.exception.JLinkException;
 import jlink.restful.java.sdk.exception.JLinkJsonException;
-import jlink.restful.java.sdk.exception.JLinkSignatureException;
-import jlink.restful.java.sdk.util.*;
+import jlink.restful.java.sdk.util.JLinkHeaderUtil;
+import jlink.restful.java.sdk.util.JLinkHttpUtil;
+import jlink.restful.java.sdk.util.JLinkLog;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * DeviceToken Request
  */
 public class DeviceTokenRequest {
-    /**
-     * @param sn devise serial number
-     * @return
-     * @throws Exception
-     */
-    public String getDeviceToken(JLinkClient jClient, String sn) {
-        //getTimestamp
-        String timeMillis = JLinkTimeMillisUtil.getTimMillis();
-        String signature = "";
-        try {
-            signature = JLinkSignatureUtil.getEncryptStr(jClient.getUuid(), jClient.getAppKey(), jClient.getAppSecret(), timeMillis, jClient.getMoveCard());
-        } catch (Exception e) {
-            JLinkLog.e("signature error " + e.getMessage());
-            throw new JLinkSignatureException(JLinkResponseCode.SIGN_ERROR.getCode(), JLinkResponseCode.SIGN_ERROR.getMsg());
-        }
-        String url = String.format("%s/%s/%s/%s.rs", JLinkDomain.AMS_DOMAIN.get(), JLinkDeviceRequestUrl.DEVICE_TOKEN.get(), timeMillis, signature);
-        JLinkAesUtil aesUtils = new JLinkAesUtil();
-        //body aes
-        DeviceTokenDto dto = new DeviceTokenDto();
-        List<String> strSn = new ArrayList<>();
-        strSn.add(sn);
-        dto.setSns(strSn);
-        JLinkLog.i("GetDeviceToken Param:" + new Gson().toJson(dto));
-        String key = aesUtils.keyFilter(timeMillis, jClient.getAppSecret());
-        //body aes string
-        String aesData = "";
-        try {
-            aesData = aesUtils.encryptToHexString(new Gson().toJson(dto), key);
-        } catch (Exception e) {
-            JLinkLog.e("aes error " + e.getMessage());
-        }
-
-        Map<String, String> headerParam = new HashMap<>();
-        headerParam.put("uuid", jClient.getUuid());
-        headerParam.put("appKey", jClient.getAppKey());
-        String deviceTokenAes = JLinkHttpUtil.httpsRequest(url, "POST", headerParam, aesData);
-        return decryptDeviceToken(deviceTokenAes, key, aesUtils);
-    }
-
     private static class DeviceTokenDto {
         /**
          * devise serial number set
@@ -86,23 +45,6 @@ public class DeviceTokenRequest {
             this.accessToken = accessToken;
         }
     }
-
-    private static String decryptDeviceToken(String deviceToken, String key, JLinkAesUtil aesUtils) {
-        try {
-            JLinkLog.i("AesDeviceToken:" + deviceToken);
-            deviceToken = aesUtils.decryptToHexString(deviceToken, key);
-            JLinkLog.i("DecryptDeviceToken:" + deviceToken);
-        } catch (Exception e) {
-            throw new JLinkException(JLinkResponseCode.AES_ERROR.getCode(), JLinkResponseCode.AES_ERROR.getMsg());
-        }
-        try {
-            DeviceTokenResponse v3 = new Gson().fromJson(deviceToken, DeviceTokenResponse.class);
-            return v3.getData().get(0).getToken();
-        } catch (Exception e) {
-            throw new JLinkJsonException(JLinkResponseCode.JSON_ERROR.getCode(), deviceToken);
-        }
-    }
-
 
     public static class DeviceTokenV3 {
 
@@ -156,6 +98,9 @@ public class DeviceTokenRequest {
         }
     }
 
+    public String getDeviceToken(JLinkClient jClient, String sn) {
+        return getDeviceToken(jClient, sn, "");
+    }
 
     public String getDeviceToken(JLinkClient jClient, String sn, String accessToken) {
         String url = String.format("%s/%s", JLinkDomain.OPENAPI_DOMAIN.get(), JLinkDeviceRequestUrl.DEVICE_TOKEN_V3.get());
